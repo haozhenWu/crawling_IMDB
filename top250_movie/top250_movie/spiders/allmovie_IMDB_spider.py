0fed1da7cc28337e5f4163539f146743a58ec4a2
haozhenWu <hwu84@wisc.edu>
2018-03-18 11:11:37 -0500

    update

--- top250_movie/top250_movie/spiders/allmovie_IMDB_spider.py
+++ top250_movie/top250_movie/spiders/allmovie_IMDB_spider.py
@@ -4,29 +4,32 @@ import os
 class loopeachmovieSpider(scrapy.Spider):
 	name = 'allmovie_IMDB'
 
-	index = range(1,8000,100)
+	index = range(1,3,1)
 	start_urls = list()
 	for page in index:
-    		start_urls.append('http://www.imdb.com/list/ls057823854/?start=' + str(page) + '&view=detail&sort=listorian:asc')
+    		#start_urls.append('http://www.imdb.com/list/ls057823854/?start=' + str(page) + '&view=detail&sort=listorian:asc')
+			start_urls.append('http://www.imdb.com/list/ls057823854/?view=detail&st_dt=&mode=detail&sort=list_order,asc&page=' + str(page))
  	def parse(self,response):
-	 
+
 	    # follow links to each movie page from top250 rated movie
-	    for href in response.css('div.info').css('b a::attr(href)').extract():
-		page = urllib2.urlopen('http://www.imdb.com/' + href)
-		page_content = page.read()
-		unique_name = "html_pages/" + href.split("/")[2] + ".html"
-		with open(unique_name, 'w') as page:
-		     page.write(page_content)
-        
-		yield scrapy.Request(response.urljoin(href),
-				     callback = self.parse_movie)
+	    for href in response.css('h3.lister-item-header').css('a::attr(href)').extract():
+            		print("\nhref is: " + href)
+			page = urllib2.urlopen('http://www.imdb.com/' + href)
+			page_content = page.read()
+			unique_name = "html_pages/" + href.split("/")[2] + ".html"
+			with open(unique_name, 'w') as page:
+			     page.write(page_content)
+
+			yield scrapy.Request(response.urljoin(href),
+					     callback = self.parse_movie)
 
 
 	def parse_movie(self,response):
-             yield {
-		'name' : response.css('div.title_wrapper').css('h1::text').extract()[0],
-		'year' : response.css('div.title_wrapper').css('span a::text').extract(),
-		'length' :response.css('div.title_wrapper').css('div.subtext').css('time::text').extract(),
+
+    		yield {
+			'name' : response.css('div.title_wrapper').css('h1::text').extract()[0],
+			'year' : response.css('div.title_wrapper').css('span a::text').extract(),
+			'length' :response.css('div.title_wrapper').css('div.subtext').css('time::text').extract(),
         	'director':response.css('div.credit_summary_item')[0].css('a span::text').extract(),
         	'writers':response.css('div.credit_summary_item')[1].css('span a span::text').extract(),
         	'stars':response.css('div.credit_summary_item')[2].css('span a span::text').extract(),
@@ -34,4 +37,3 @@ class loopeachmovieSpider(scrapy.Spider):
         	'Description':response.css('div.plot_summary').css('div.summary_text').css('div::text').extract()
 
 		}
-
